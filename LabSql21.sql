d6f2fa03f69a295ef2003e0fafa4cd6cff834094
phurinatNT <phurinatssx@gmail.com>
2025-08-21 10:55:09 +0700

    Update LabSql21.sql

--- LabSql21.sql
+++ LabSql21.sql
@@ -50,3 +50,54 @@ select p.ProductID,p.ProductName,sum(Quantity) as จำนวนที่ขา
 from Products p JOIN [Order Details] od on p.ProductID = od.ProductID
 Group by p.ProductID,p.ProductName
 order by 1
+
+select o.OrderID เลขใบสั่งซื้อ, c.CompanyName ลูกค้า,
+		e.FirstName พนักงาน, o.ShipAddress ส่งไปที่
+from Orders o,Customers c,Employees e
+where o.CustomerID = c.CustomerID
+And o.EmployeeID = e.EmployeeID
+
+select o.OrderID เลขใบสั่งซื้อ, c.CompanyName ลูกค้า,
+		e.FirstName พนักงาน, o.ShipAddress ส่งไปที่
+from Orders o
+	join Customers c on o.CustomerID = c.CustomerID
+	join Employees e on o.EmployeeID = e.EmployeeID
+
+select e.EmployeeID, FirstName, count(*) as [จำนวน order]
+		,sum(freight) as [Sum of Freight]
+from Employees e join Orders o on e.EmployeeID = o.EmployeeID
+where year(OrderDate) = 1998
+Group by e.EmployeeID, FirstName
+Order by 3 desc
+
+--ต้องการรหัสสินค้า ชื่อสินค้าที่ nancy ขายได้ ทั้งหมด เรียงตามลำดับรหัสสินค้า
+select p.ProductID, p.ProductName
+from Employees e join orders o on e.EmployeeID = o.EmployeeID
+				 join [Order Details] od on o.OrderID = od.OrderID
+				 join Products p on od.ProductID = p.ProductID
+where e.FirstName = 'Nancy' 
+Group by p.ProductID, p.ProductName 
+order by ProductID
+
+select distinct p.ProductID, p.ProductName
+from Employees e join orders o on e.EmployeeID = o.EmployeeID
+				 join [Order Details] od on o.OrderID = od.OrderID
+				 join Products p on od.ProductID = p.ProductID
+where e.FirstName = 'Nancy' 
+order by ProductID
+--ต้องการชื่อบริษัทลูกค้า around the Horn ซื้อสินค้าที่มาจากประเทศอะไรบ้าง
+select distinct s.Country
+from Customers c join orders o on c.CustomerID = o.CustomerID
+				 join [Order Details] od on o.OrderID = od.OrderID
+				 join Products p on od.ProductID = p.ProductID
+				 join Suppliers s on s.SupplierID = p.SupplierID
+where c.CompanyName = 'Around the Horn'
+--บริษัทลูกค้าชื่อ Around the Horn ซื้อสินค้าอะไรบ้าง จำนวนเท่าใด
+select p.ProductID, p.ProductName, sum(Quantity) [sum of Quantity]
+from Customers c join orders o on c.CustomerID = o.CustomerID
+				 join [Order Details] od on od.OrderID = o.OrderID
+				 join Products p on p.ProductID = od.ProductID
+where c.CompanyName = 'Around the Horn'
+Group by p.ProductID, p.ProductName
+order by 1
+
